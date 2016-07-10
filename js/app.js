7206b0a0befae65f29cf1e8920a2300f3169be28
Chavah Jacobs <chavah.jacobs@greensmoke.com>
2016-07-10 13:33:54 +0300

    Fixed infowindows a little, still not list/marker connection

--- js/app.js
+++ js/app.js
@@ -11,84 +11,84 @@ var allStops = [
         lat: 31.714564,
         lng: 34.990076,
         streets: ['arugot', 'kishon', 'sorek', 'uriya', 'miha'],
-        description: "Just above the the corner of Kishon and Sorek. The first stop. ETA is 8:40 AM."
+        description: "<strong>First Stop:</strong> Just above the the corner of Kishon and Sorek. ETA is 8:40 AM."
     },
     {
         name: "Bar Col",
         lat: 31.714856,
         lng: 34.993992,
         streets: ['sorek', 'nahshon', 'lakhish'],
-        description: "Just after the corner where the bottom of Lachish and Sorek meet. The second stop. ETA is 8:41 AM."
+        description: "<strong>Second Stop:</strong> Just after the corner where the bottom of Lachish and Sorek meet. ETA is 8:41 AM."
     },
     {
         name: "Top of Ayalon Park",
         lat: 31.713816,
         lng: 34.996277,
         streets: ['sorek', 'lakhish'],
-        description: "Just after where the top of Lachish intersects with Sorek. The third stop. ETA is 8:42 AM."
+        description: "<strong>Third Stop:</strong> Just after where the top of Lachish intersects with Sorek. ETA is 8:42 AM."
     },
     {
         name: "Top of Ayalon Street",
         lat: 31.712900,
         lng: 34.997843,
         streets: ['ayalon', 'dolev', 'achzib', 'refa\'im'],
-        description: "Where Ayalon and Dolev intersect, near Best Market. The fourth stop. ETA is 8:43 AM."
+        description: "<strong>Fourth Stop:</strong> Where Ayalon and Dolev intersect, near Best Market. ETA is 8:43 AM."
     },
     {
         name: "North Dolev",
         lat: 31.715687,
         lng: 34.998155,
         streets: ['dolev', 'katlav', 'ktalav', 'timna'],
-        description: "Just before Dolev and Katlav meet. The fifth stop. ETA is 8:44 AM."
+        description: "<strong>Fifth Stop:</strong> Just before Dolev and Katlav meet. ETA is 8:44 AM."
     },
     {
         name: "Top of Dolev",
         lat: 31.712247,
         lng: 34.999441,
         streets: ['dolev', 'mata', 'shimshon', 'el al', 'alexander'],
-        description: "Just before the corner of Dolev and Shimshon. The sixth stop. ETA is 8:45 AM."
+        description: "<strong>Sixth Stop:</strong> Just before the corner of Dolev and Shimshon. ETA is 8:45 AM."
     },
     {
         name: "Bottom of Shimshon",
         lat: 31.714460,
         lng: 35.000503,
         streets: ['shimshon', 'hayarden'],
-        description: "Just before Shimshon intersects with HaYarden. The seventh stop. ETA is 8:46 AM."
+        description: "<strong>Seventh Stop:</strong> Just before Shimshon intersects with HaYarden. ETA is 8:46 AM."
     },
     {
         name: "HaYarden",
         lat: 31.710867,
         lng: 35.001394,
         streets: ['hayarden', 'refa\'im', 'maor'],
-        description: "Just after the corner of HaYarden and Refa'im. The eighth stop. ETA is 8:46 AM."
+        description: "<strong>Eighth Stop:</strong> Just after the corner of HaYarden and Refa'im. ETA is 8:46 AM."
     },
     {
         name: "HaYarkon",
         lat: 31.707981,
         lng: 34.998087,
         streets: ['hayarkon', 'luz'],
-        description: "At the midpoint between HaYarden and Luz. The ninth stop. ETA is 8:47 AM."
+        description: "<strong>Ninth Stop:</strong> At the midpoint between HaYarden and Luz. ETA is 8:47 AM."
     },
     {
         name: "Bottom of Ayalon Park",
         lat: 31.711840,
         lng: 34.990501,
         streets: ['kishon', 'ayalon', 'no\'am', 'gilo', 'ramot', 'luz', 'shaham'],
-        description: "After the corner of Ayalon and Kishon. The tenth stop. ETA is 8:48 AM."
+        description: "<strong>Tenth Stop:</strong> After the corner of Ayalon and Kishon. ETA is 8:48 AM."
     },
     {
         name: "Grill Burger",
         lat: 31.712675,
         lng: 34.988595,
         streets: ['kishon', 'shaham', 'tse\'elim', 'tsefat', 'arugot'],
-        description: "On the corner of Kishon and Tse'elim. The eleventh stop. ETA is 8:49 AM."
+        description: "<strong>Eleventh Stop:</strong> On the corner of Kishon and Tse'elim. ETA is 8:49 AM."
     },
     {
         name: "Hever",
         lat: 31.713287,
         lng: 34.982389,
         streets: ['tse\'elim', 'hever', 'ein gedi', 'hebron', 'kiryat arba', 'tiberias'],
-        description: "After the roundabout where Tse'elim and Hever intersect. The final stop. ETA is 8:50 AM."
+        description: "<strong>Final Stop:</strong> After the roundabout where Tse'elim and Hever intersect. ETA is 8:50 AM."
     }
 ];
 
@@ -123,7 +123,7 @@ var Stop = function(data) {
     });
 
     marker.addListener('click', function() {
-        infowindow.setContent(data.description);
+        infowindow.setContent('<h3>'+data.name+'</h3>' + '<p>' + data.description + '</p>');
         infowindow.open(map, this);
     });
 
@@ -142,15 +142,15 @@ var ViewModel = function() {
     self.allStops = ko.observableArray([]);
     self.filteredStops = ko.observableArray([]);
     self.markers = [];
-    self.currentStop = ko.observable();
-    self.searchStops = ko.observable('');
+    self.currentStop = self.allStops[0];
+    self.searchOfStops = ko.observable(""); //holds query
 
     allStops.forEach(function(item){
             self.allStops.push( new Stop(item) );
         });
 
     self.filteredStops = ko.computed(function () {
-        var search = self.searchStops().toLowerCase();
+        var search = self.searchOfStops().toLowerCase();
         if(!search) {
             return self.allStops();
         } else {
@@ -166,25 +166,6 @@ var ViewModel = function() {
         }
     }, self);
 
-    // self.filteredStops = ko.computed(function () {
-    //     var search = self.searchStops().toLowerCase();
-    //         return ko.utils.arrayFilter(self.allStops(), function(item) {
-    //             item.streets.forEach(function(street){
-    //                 if (street.indexOf(search) > -1) {
-    //                     item.marker.setVisible(true);
-    //                     return true;
-    //                 } else if (item.name().indexOf(search) > -1) {
-    //                     item.marker.setVisible(true);
-    //                     return true;
-    //                 } else {
-    //                     item.marker.setVisible(false);
-    //                     return false;
-    //                 }
-    //             });
-    //         }
-    //     }, self);
-
-    //determine which infoWindow is showing and which marker is bouncing
     self.setCurrentStop = function(data) {
         self.currentStop(data);
     };
